eb60442e63ca9a913105ebe5df50f7b956e860ca
haoran <haoran.mc@outlook.com>
2025-02-17 00:01:28 +0800

    manual parsing of network packets by protocol
    read origin packet data rather packet source

---
+++ input/listen_libpcap.go
@@ -0,0 +1,71 @@
+package input
+
+import (
+	"io"
+	"log"
+	"net"
+	"syscall"
+	"time"
+
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+	"github.com/haoran-mc/sniffer/setting"
+)
+
+func Listen(nic string) {
+	// 获取网卡信息
+	iface, err := net.InterfaceByName(nic)
+	if err != nil {
+		log.Fatalf("NIC %s not found, error: %v", setting.App.Nic, err)
+	}
+	log.Printf("NIC: %s, MTU: %d", nic, iface.MTU)
+
+	// 打开设备监听
+	handle, err := pcap.OpenLive(
+		nic,                // Nic
+		9000,               // SnapLen
+		true,               // Promisc
+		1*time.Millisecond, // Timeout
+	)
+	if err != nil {
+		log.Fatalf("OpenLive %s error: %v", setting.App.Nic, err)
+	}
+
+	// 设置过滤
+	if err := handle.SetBPFFilter(setting.App.Bpf); err != nil {
+		log.Fatalf("Set BPF Filter error: %v", err)
+	}
+
+	if handle.LinkType() != layers.LinkTypeEthernet {
+		log.Fatal("Handle link type is not ethernet")
+	}
+
+	go func() {
+		defer handle.Close()
+		pcktChanLen := cap(pcktChan)
+
+		for {
+			data, captureInfo, err := handle.ReadPacketData()
+			if err == nil {
+				if len(pcktChan) < pcktChanLen {
+					pcktChan <- &pckt{data, &captureInfo}
+				} // else queue drop
+				continue
+			}
+			if enext, ok := err.(pcap.NextError); ok && enext == pcap.NextErrorTimeoutExpired {
+				continue
+			}
+			if eno, ok := err.(syscall.Errno); ok && eno.Temporary() {
+				continue
+			}
+			if enet, ok := err.(*net.OpError); ok && (enet.Temporary() || enet.Timeout()) {
+				continue
+			}
+			if err == io.EOF || err == io.ErrClosedPipe {
+				log.Fatalf("stopped reading from %s interface with error: %s\n", nic, err)
+			}
+
+			log.Fatalf("stopped reading from %s interface with error: %s\n", nic, err)
+		}
+	}()
+}
--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"log"
-
-	"github.com/haoran-mc/sniffer/packet"
+	"github.com/haoran-mc/sniffer/input"
 	"github.com/haoran-mc/sniffer/setting"
 )
 
 func main() {
-	if err := packet.Listen(setting.App.Nic); err != nil {
-		log.Println(err.Error())
-	}
+	go input.Listen(setting.App.Nic)
+
+	select {}
 }
